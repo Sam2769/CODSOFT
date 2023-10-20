768e978bc5cdeba61d1155f9c164750c364e67af
Saksham Verma <sakshamverma1000@gmail.com>
2023-10-20 17:38:04 +0530

    initial comment

---
+++ C++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+
+    int a,b;
+    cout<<"enter a: "<< endl;
+    cin>> a;
+    cout<< endl;
+
+    cout<<"Enter b: "<< endl;
+    cin>> b;
+    cout<< endl;
+
+    cout<<"The sum of a and b is = "<< a +b << endl;
+ 
+   return 0;
+ 
+   }
