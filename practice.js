82b7d4907f9411f52ae3d88b7f3a4f872be907bf
Saksham Verma <sakshamverma1000@gmail.com>
2023-11-03 17:28:20 +0530

    git push

--- practice.js
+++ practice.js
@@ -2,18 +2,22 @@ const http = require("http");
 
 const result = [1, 2, 3, 4];
 
-const server = http.createServer(function (_req, res) {
-  if ((res.url = "/")) {
-    res.write(result);
-  } else if ((req.url = "/create")) {
-    result.push(Math.random() * 100);
-  } else if ((req.url = "/update")) {
+const server = http.createServer(function (req, res) {
+  if (req.url === "/") {
+    //nothing to do here
+    res.write(JSON.stringify(result));
+  } else if (req.url === "/create") {
+    result.push(math.random() * 100);
+    res.write(JSON.stringify(result));
+  } else if (req.url === "/update") {
     result[0]++;
-  } else if (req.url == "/delete") {
+    res.write(JSON.stringify(result));
+  } else if (req.url === "/delete") {
     result.pop();
+    res.write(JSON.stringify(result));
   }
 
   res.end();
 });
 
-server.listen(3000, () => console.log("listening on port 3000"));
+server.listen(5500, () => console.log("listening on Port 5500"));
